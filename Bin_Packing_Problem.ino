9e3e9bebb52ca11d3d641420d18b98e8dfdeb4e0
Danny van Bokhorst <dannyvanbokhorst@live.nl>
2019-04-23 13:48:40 +0200

    Bin packaging problem code aangemaakt
    Veel oude code weggehaald, omdat we nu anders werken. Meteen voor gezorgd dat er geneog serial.reads inzitten, zodat je goed kan zien dat alles goed gaat. Later komt er nog een bestand voor Serial.read en met Java erbij.

--- Bin_Packing_Problem.ino
+++ Bin_Packing_Problem.ino
@@ -7,47 +7,112 @@ int servoPin2 = 12;
 Servo Servo1; 
 Servo Servo2;
 
-int blok = 2;
-int doosLinks = 10;
-int doosRechts = 10;
+int blok = 3;
+char blokKleur = '2'; // Als het nummer van de Java Applicatie voor servoKleur 1 is, is de kleur blauw. Als het 2 is, is de kleur rood.
+
+int doosLinksBlauw = 10;
+int doosRechtsBlauw = 10;
+
+int doosLinksRood = 10;
+int doosRechtsRood = 10;
 
 void setup() {
-   Servo1.attach(servoPin1); 
-   Servo2.attach(servoPin2); 
+   Servo1.attach(servoPin1);
+   Servo2.attach(servoPin2);
+   Serial.begin(9600);
+   Serial.println("Setup klaar");
+}
+
+void testServos() {
+   Servo1.write(0);  //links
+   Servo2.write(0);  //links
+   Serial.println("links");
+   delay(3000);
+   
+   Servo1.write(90);  //rechts
+   Servo2.write(90);  //rechts
+   Serial.println("rechts");
+   delay(3000);
 }
 
 void loop() {
-
- if(blok == doosLinks){ //past precies in links
-  doosLinks = doosLinks - blok; //nieuwe max inhoud
   
-  Servo1.write(0);  //rechts
- }
- else if(blok == doosRechts){ //past precies in rechts
-  doosRechts = doosRechts - blok; //nieuwe max inhoud
+   //testServos();
+   
+   if(blokKleur == '1') {
+      Serial.print("Blok: ");
+      Serial.println(blok);
+      
+      Serial.print("doosLinksBlauw: ");
+      Serial.println(doosLinksBlauw);
+      
+      Serial.print("doosRechtsBlauw: ");
+      Serial.println(doosRechtsBlauw);
+      Serial.println();
+      delay(3000);
+    
+   } else if (blokKleur == '2') {
+      Serial.print("Blok: ");
+      Serial.println(blok);
+      
+      Serial.print("doosLinks: ");
+      Serial.println(doosLinksRood);
+      
+      Serial.print("doosRechts: ");
+      Serial.println(doosRechtsRood);
+      Serial.println();
+      delay(3000);
+   }
 
-  Servo1.write(45); //links
- }
- else if(doosLinks <= doosRechts && blok < doosLinks){ // links is kleiner dan rechts
-  doosLinks = doosLinks - blok; //nieuwe max inhoud
-  
-  Servo1.write(0);  //rechts
- }
- else if(doosRechts < doosLinks && blok < doosRechts){ // rechts is kleiner dan links
-  doosRechts = doosRechts - blok; //nieuwe max inhoud
+  if(blokKleur == '1') { // Kleur is blauw
+    
+    Serial.println("De kleur van de blok is blauw!");
+    
+    if(doosLinksBlauw - blok >= 0) {
 
-  Servo1.write(45); //links
- }
-else if(blok > doosLinks && blok < doosRechts){ // blok past niet in links
-  doosRechts = doosRechts - blok; //nieuwe max inhoud
+      Serial.println("De blauwe blok gaat naar links!");
+      Serial.println();
+      doosLinksBlauw = doosLinksBlauw - blok;
+      Servo1.write(0);  // Servo motor naar rechts, blok gaat naar linkerdoos.
+      delay(1000);
+      
+    } else if (doosRechtsBlauw - blok >= 0) {
 
-  Servo1.write(45); //links
-}
-else if(blok > doosRechts && blok < doosLinks){ // blok past niet in rechts
-  doosLinks = doosLinks - blok; //nieuwe max inhoud
-  
-  Servo1.write(0);  //rechts
-}else if(blok > doosLinks && blok > doosRechts){ // dozen zijn vol
-  Serial.println(4);
-}
+      Serial.println("De blauwe blok gaat naar rechts!");
+      Serial.println();
+      doosRechtsBlauw = doosRechtsBlauw - blok;
+      Servo1.write(45);  // Servo motor naar links, blok gaat naar rechterdoos.
+      delay(1000);
+      
+    } else if (doosLinksBlauw - blok < 0 && doosRechtsBlauw - blok < 0) { // Dozen zijn beide vol.
+      Serial.println("Dozen zijn vol!"); // Laat doos vol melding zien en stopt het programma.
+      Serial.println();
+      delay(10000);
+    }
+    
+  } else if (blokKleur == '2') { // Kleur is rood
+    Serial.println("De kleur van de blok is rood!");
+
+    if(doosLinksRood - blok >= 0) {
+
+      Serial.println("De rode blok gaat naar links!");
+      Serial.println();
+      doosLinksRood = doosLinksRood - blok;
+      Servo2.write(0);  // Servo motor naar rechts, blok gaat naar linkerdoos.
+      delay(1000);
+      
+    } else if (doosRechtsRood - blok >= 0) {
+
+      Serial.println("De blauw blok gaat naar rechts!");
+      Serial.println();
+      doosRechtsRood = doosRechtsRood - blok;
+      Servo2.write(45);  // Servo motor naar links, blok gaat naar rechterdoos.
+      delay(1000);
+      
+    } else if (doosLinksRood - blok < 0 && doosRechtsRood - blok < 0) { // Dozen zijn beide vol.
+      Serial.println("Dozen zijn vol!"); // Laat doos vol melding zien en stopt het programma.
+      Serial.println();
+      delay(10000);
+    }
+  }
 }
