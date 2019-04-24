e8e7104a217a4946beb959f5964c5114b3f53ac0
iYannick0 <43132788+iYannick0@users.noreply.github.com>
2019-04-24 11:36:09 +0200

    Update Bin_Packing_Problem_with_java_and_serial_read.ino

--- Bin_Packing_Problem_with_java_and_serial_read.ino
+++ Bin_Packing_Problem_with_java_and_serial_read.ino
@@ -1,5 +1,11 @@
 // Include the Servo library 
 #include <Servo.h> 
+
+#define motorsnelheid1 4
+#define motorrichting1 5
+#define motorsnelheid2 6
+#define motorrichting2 7
+
 // Declare the Servo pin 
 int servoPin1 = 13;
 int servoPin2 = 12; 
@@ -7,8 +13,8 @@ int servoPin2 = 12;
 Servo Servo1; 
 Servo Servo2;
 
-int blok = 3;
-char blokKleur = '2'; // Als het nummer van de Java Applicatie voor servoKleur 1 is, is de kleur blauw. Als het 2 is, is de kleur rood.
+int blok;
+char blokKleur; // Als het nummer van de Java Applicatie voor servoKleur 1 is, is de kleur blauw. Als het 2 is, is de kleur rood.
 byte Status;
 
 int doosLinksBlauw = 10;
@@ -16,6 +22,8 @@ int doosRechtsBlauw = 10;
 
 int doosLinksRood = 10;
 int doosRechtsRood = 10;
+
+int i;
 
 void setup() {
    Servo1.attach(servoPin1);
@@ -32,7 +40,25 @@ void loop() {
   }
   
   if (Status == 1) {
+    if (i < 1) {
+      analogWrite(motorsnelheid1, 210);
+      digitalWrite(motorrichting1, HIGH);
+      analogWrite(motorsnelheid2, 210);
+      digitalWrite(motorrichting2, HIGH);
+      i++;
+}
    
+   if(Status == 3){ //Kleur is blauw
+    blokKleur = 1;
+   }
+   if(Status == 4){ //Kleur is rood
+    blokKleur = 2;
+   }
+
+   if(Status == 5){ //Grootte van het blok 
+    blok = Serial.read();
+   }
+    
    if(blokKleur == '1') {
       Serial.print("Blok: ");
       Serial.println(blok);
@@ -58,29 +84,29 @@ void loop() {
       delay(3000);
    }
 
-  if(blokKleur == '1') { // Kleur is blauw
+  if(blokKleur == '1') { //Kleur is blauw
     
-    Serial.println("De kleur van de blok is blauw!");
+    Serial.println("De kleur van het blok is blauw!");
     
     if(doosLinksBlauw - blok >= 0) {
 
-      Serial.println("De blauwe blok gaat naar links!");
-      Serial.println();
+      Serial.println("Het blauwe blok gaat naar links!");
+      Serial.println("8");
       doosLinksBlauw = doosLinksBlauw - blok;
       Servo1.write(0);  // Servo motor naar rechts, blok gaat naar linkerdoos.
       delay(1000);
       
     } else if (doosRechtsBlauw - blok >= 0) {
 
-      Serial.println("De blauwe blok gaat naar rechts!");
-      Serial.println();
+      Serial.println("Het blauwe blok gaat naar rechts!");
+      Serial.println("9");
       doosRechtsBlauw = doosRechtsBlauw - blok;
       Servo1.write(45);  // Servo motor naar links, blok gaat naar rechterdoos.
       delay(1000);
       
     } else if (doosLinksBlauw - blok < 0 && doosRechtsBlauw - blok < 0) { // Dozen zijn beide vol.
       Serial.println("Dozen zijn vol!"); // Laat doos vol melding zien en stopt het programma.
-      Serial.println();
+      Serial.println("4");
       delay(10000);
     }
     
@@ -89,25 +115,34 @@ void loop() {
 
     if(doosLinksRood - blok >= 0) {
 
-      Serial.println("De rode blok gaat naar links!");
-      Serial.println();
+      Serial.println("Het rode blok gaat naar links!");
+      Serial.println("6");
       doosLinksRood = doosLinksRood - blok;
       Servo2.write(0);  // Servo motor naar rechts, blok gaat naar linkerdoos.
       delay(1000);
       
     } else if (doosRechtsRood - blok >= 0) {
 
-      Serial.println("De blauw blok gaat naar rechts!");
-      Serial.println();
+      Serial.println("Het rode blok gaat naar rechts!");
+      Serial.println("7");
       doosRechtsRood = doosRechtsRood - blok;
       Servo2.write(45);  // Servo motor naar links, blok gaat naar rechterdoos.
       delay(1000);
       
     } else if (doosLinksRood - blok < 0 && doosRechtsRood - blok < 0) { // Dozen zijn beide vol.
       Serial.println("Dozen zijn vol!"); // Laat doos vol melding zien en stopt het programma.
-      Serial.println();
+      Serial.println("5");
       delay(10000);
     }
   }
   }
+
+if (Status == 2) {
+    analogWrite(motorsnelheid1, 0);
+    digitalWrite(motorrichting1, HIGH);
+    analogWrite(motorsnelheid2, 0);
+    digitalWrite(motorrichting2, HIGH);
+    i = 0;
+}
+
 }
